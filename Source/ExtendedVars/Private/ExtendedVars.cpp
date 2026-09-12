8fb5712147c152741b4839bb8426e6fc49c7bd82
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2026-09-13 02:34:38 +0300

    Update ExtendedVars.cpp

--- Source/ExtendedVars/Private/ExtendedVars.cpp
+++ Source/ExtendedVars/Private/ExtendedVars.cpp
@@ -6,15 +6,12 @@
 
 void FExtendedVarsModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	
 }
 
 void FExtendedVarsModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-	
+
 }
 
 #undef LOCTEXT_NAMESPACE
